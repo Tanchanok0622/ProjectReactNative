7c8d1e1710ae9231eec50ed78dde538ab8191111
Tanchanok Ngamjaturong <94946248+Tanchanok0622@users.noreply.github.com>
2021-12-15 11:43:50 +0700

    Add files via upload

---
+++ 15-12-2021/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import ModelExample from './components/ModelExample'
+//import AlertExample from './components/AlertExample'
+
+const App = () => {
+  return (
+
+      //<AlertExample/>
+      <ModelExample/>
+
+  )
+}
+
+export default App
